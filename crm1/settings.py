217b707bfd5a00dd4ef7bc7e383d540620b08d05
devndataai <mjpythoncode@gmail.com>
2026-06-21 12:11:40 +0530

    0.3
    0.3

--- crm1/settings.py
+++ crm1/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = "cz=&%f*9(d*zo$_55p=(p)(eki#p$pb^0159-)8k^6$9c3l&_b"
 DEBUG = True  # Set to False in production
 
 ALLOWED_HOSTS = [
+    "*"
     "localhost",
     "127.0.0.1",
     "yourdomain.com",
@@ -38,6 +39,7 @@ ALLOWED_HOSTS = [
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
+    ".onrender.com",
     "apmindataai.onrender.com",
 ]
 
